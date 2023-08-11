664533d5c130db135377504def89b727bbe24601
francogiaccone <francogiaccone92003@gmail.com>
2023-08-11 18:19:21 -0300

    Clase main

--- src/ejerciciorobot/EjercicioRobot.java
+++ src/ejerciciorobot/EjercicioRobot.java
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejerciciorobot;
 
-/**
- *
- * @author franc
- */
 public class EjercicioRobot {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Robot robot = new Robot();
+        
+        Hombre hombre1 = new Hombre();
+        
+        hombre1.jugarConRobot(robot);
+        
+        Hombre hombre2 = new Hombre();
+        
+        hombre2.jugarConRobot(robot);
     }
     
 }
