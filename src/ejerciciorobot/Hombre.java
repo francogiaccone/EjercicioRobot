0f048df6169a74706b33efc0521228b89af6242f
IvanM1995 <142050290+IvanM1995@users.noreply.github.com>
2023-08-11 18:06:07 -0300

    Clasehom

--- src/ejerciciorobot/Hombre.java
+++ src/ejerciciorobot/Hombre.java
@@ -3,11 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package ejerciciorobot;
 
 /**
  *
  * @author Ivan
  */
 public class Hombre {
+    
+    public void jugarConRobot(Robot robot1) {
+        robot1.avanzar(500);
+        robot1.retroceder(20);
+        System.out.println("Energia actual: " + robot1.energiaActual());
+        robot1.dormir();
+    }
     
 }
