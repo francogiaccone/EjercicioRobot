1bcafaf7150ef2fb7600338bdf97bd746fbc31f5
francogiaccone <francogiaccone92003@gmail.com>
2023-08-11 17:50:29 -0300

    Initial commit

---
+++ src/ejerciciorobot/Bateria.java
@@ -0,0 +1,21 @@
+package ejerciciorobot;
+
+public class Bateria {
+    
+    //atributos
+    private int carga;
+
+    public Bateria() {
+        this.carga = 1000;
+    }
+    
+    //get and set
+    public int getCarga() {
+        return carga;
+    }
+
+    public void setCarga(int carga) {
+        this.carga = carga;
+    }
+
+}
